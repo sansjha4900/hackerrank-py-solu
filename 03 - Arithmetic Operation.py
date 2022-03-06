b76e404093c34f6d63ca5786f31e4448b2e849f0
Sanskriti Jha <sanskriti.jha@live.in>
2022-03-06 10:11:06 +0530

    Update 03 - Arithmetic Operation.py

--- 03 - Arithmetic Operation.py
+++ 03 - Arithmetic Operation.py
@@ -3,14 +3,13 @@
   
 # Task:
 # The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
-
-## The first line contains the sum of the two numbers.
-## The second line contains the difference of the two numbers (first - second).
-## The third line contains the product of the two numbers.
+#   The first line contains the sum of the two numbers.
+#   The second line contains the difference of the two numbers (first - second).
+#   The third line contains the product of the two numbers.
 
 # Input Format:
-# The first line contains the first integer, a.
-# The second line contains the second integer, b.
+#   The first line contains the first integer, a.
+#   The second line contains the second integer, b.
 
 if __name__ == '__main__':
     a = int(input())
