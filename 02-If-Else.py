812ef2003459905448ccdda61e9401dbdd94a83c
Sanskriti Jha <sanskriti.jha@live.in>
2022-03-06 09:54:37 +0530

    Update and rename If-Else.py to 02-If-Else.py

---
+++ 02-If-Else.py
@@ -0,0 +1,29 @@
+# Ques 2:~ If-Else
+# (Score = 10, Difficulty = Easy)
+
+# Task:
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+import math
+import os
+import random
+import re
+import sys
+
+n = int(input())
+if n % 2 == 0:
+    if n in range(2,6):
+        print("Not Weird")
+
+    elif n in range(6,21):
+        print("Weird")
+
+    elif n > 20:
+        print("Not Weird")
+else:
+    print("Weird")
