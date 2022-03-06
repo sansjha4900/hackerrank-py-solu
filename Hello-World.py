fceb24257baeb7ba3f327b965b5aaf7e9ef3ccd3
Sanskriti Jha <sanskriti.jha@live.in>
2022-03-06 09:33:39 +0530

    Update Hello-World.py

--- Hello-World.py
+++ Hello-World.py
@@ -1,4 +1,5 @@
-# Ques 1:~
+# Ques 1:~ Hello World
+# (Score = 5, Difficulty = Easy)
 
 # Here is a sample line of code that can be executed in Python: print("Hello, World!")
 # You can just as easily store a string as a variable and then print it to stdout:
